fd893aa3eac3a757e5252000c1ec68056578ddb2
Stefan Smudja <stefan.smudja@consid.se>
2023-12-01 15:55:50 +0100

    2022 Day 3 PT2 solution

--- src/main/kotlin/aoc2022/day3/Main.kt
+++ src/main/kotlin/aoc2022/day3/Main.kt
@@ -7,10 +7,13 @@ val items: List<Char> = ('a'..'z').toList()
 
 typealias Item = Char
 typealias Compartments = Pair<String, String>
+typealias Group = ArrayList<String>
 fun main() {
     val data = readFileLines("/Users/stefanconsid/IdeaProjects/AdventOfCode-Day1/src/main/kotlin/aoc2022/day3/data.txt")
     // P1
-    println("Sum of priority points is: ${solutionPart1(data)}")
+    //println("Sum of priority points is: ${solutionPart1(data)}")
+    // P2
+    println("Sum of priority points is: ${solutionPart2(data)}")
 }
 
 fun solutionPart1(dataSet: Stream<String>): Int {
@@ -48,4 +51,35 @@ fun String.splitInMiddle(): Compartments {
         first = this.substring(0, this.length / 2),
         second = this.substring(this.length / 2, this.length)
     )
+}
+
+fun solutionPart2(dataSet: Stream<String>): Int {
+    var prioritySum = 0
+    val group = Group()
+    var rowNumber = 1
+
+    dataSet.forEachOrdered { ruckSack ->
+        group.add(ruckSack)
+
+        if (rowNumber % 3 == 0) {
+            var match: Item? = null
+
+            group[0].forEach { c ->
+                if (group[1].any { it == c } && group[2].any { it == c })
+                    match = c
+            }
+
+            match?.let { unwrappedMatch ->
+                println("Match '$unwrappedMatch'")
+                prioritySum += getPointsForItem(unwrappedMatch)
+            }
+
+            // Wipe group
+            group.clear()
+        }
+
+        rowNumber++
+    }
+
+    return prioritySum
 }
