8f9a940a133d5988bd18e5f33c6580db607f0d99
Stefan Smudja <stefan.smudja@consid.se>
2023-12-18 16:37:10 +0100

    yolo

--- src/main/kotlin/extensions/StreamExtensions.kt
+++ src/main/kotlin/extensions/StreamExtensions.kt
@@ -2,6 +2,9 @@ package extensions
 
 import java.util.stream.Stream
 
+/**
+ * @return [List] of [String] from a [Stream] of [String]
+ */
 fun Stream<String>.aocToList(): List<String> {
     val mutableList = mutableListOf<String>()
     this.forEachOrdered {
